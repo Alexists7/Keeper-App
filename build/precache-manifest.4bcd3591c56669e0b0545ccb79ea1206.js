90b39bc86c321ac88155d3e5e5d7e00619cca0ad
SHAJU THOMAS <alexisshaju@gmail.com>
2021-04-15 18:17:47 +0100

    Ok

---
+++ build/precache-manifest.4bcd3591c56669e0b0545ccb79ea1206.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c7c6fc2ae68080d65a46ae46117397",
+    "url": "/Alexists7/Keeper-App/index.html"
+  },
+  {
+    "revision": "889738c9c75187b3eb1d",
+    "url": "/Alexists7/Keeper-App/static/js/2.8282a2d3.chunk.js"
+  },
+  {
+    "revision": "81aabd50ebf8fa0f91e4",
+    "url": "/Alexists7/Keeper-App/static/js/main.8a710d84.chunk.js"
+  },
+  {
+    "revision": "9babe7d0c5b32a6c6d6f",
+    "url": "/Alexists7/Keeper-App/static/js/runtime-main.e9c75700.js"
+  }
+]);
